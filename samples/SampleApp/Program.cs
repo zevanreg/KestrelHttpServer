11b9f8bf97aa9a5942b02b9defb848d3ab0685b7
Louis DeJardin <lodejard@microsoft.com>
2015-07-20 18:44:40 -0700

    Sample gets a tight loop around action queue

--- samples/SampleApp/Program.cs
+++ samples/SampleApp/Program.cs
@@ -1,7 +1,11 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNet.Server.Kestrel.Networking;
+using Microsoft.Framework.Runtime;
 using System;
+using System.Diagnostics;
+using System.Threading;
 
 namespace SampleApp
 {
@@ -16,8 +20,43 @@ namespace SampleApp
 
         public void Main(string[] args)
         {
+            
+            var w = new Worker((ILibraryManager)_services.GetService(typeof(ILibraryManager)));
+            w.Start();
+
+            WaitCallback threadPoolCallback = _ => { };
+            var ticks = 0;
+            Action postCallback = () =>
+            {
+                ThreadPool.QueueUserWorkItem(threadPoolCallback);
+                //ThreadPool.UnsafeQueueUserWorkItem(threadPoolCallback, null);
+            };
+
+            threadPoolCallback = _ =>
+            {
+                w.Post(postCallback);
+                Interlocked.Increment(ref ticks);
+            };
+
+            for (var x = 0; x != 1000; ++x)
+            {
+                w.Post(postCallback);
+            }
+
+            var thread = new Thread(() =>
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                for (; ;)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    Console.WriteLine("{0} {1}/sec", ticks, ticks / sw.Elapsed.TotalSeconds);
+                }
+            });
+            thread.Start();
+            
             new Microsoft.AspNet.Hosting.Program(_services).Main(new[] {
-                "--server","kestrel"
+                "--server","Kestrel"
             });
         }
     }
---
+++ samples/SampleApp/Worker.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNet.Server.Kestrel;
+using Microsoft.AspNet.Server.Kestrel.Networking;
+using Microsoft.Framework.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace SampleApp
+{
+    internal class Worker
+    {
+        readonly Thread _thread;
+        readonly KestrelEngine _engine;
+        readonly Libuv _uv;
+        readonly UvLoopHandle _loop;
+        readonly UvAsyncHandle _async;
+        readonly List<Action> _actions;
+
+        public Worker(ILibraryManager lm)
+        {
+            _thread = new Thread(ThreadStart);
+            _engine = new KestrelEngine(lm);
+            _uv = _engine.Libuv;
+            _loop = new UvLoopHandle();
+            _async = new UvAsyncHandle();
+            _actions = new List<Action>();
+        }
+
+        public void Start()
+        {
+            var wait = new ManualResetEvent(false);
+            _thread.Start(wait);
+            wait.WaitOne();
+        }
+
+        private void ThreadStart(object w)
+        {
+            _loop.Init(_uv);
+            _async.Init(_loop, OnAsync);
+            ((ManualResetEvent)w).Set();
+            _loop.Run();
+        }
+
+        public void Post(Action action)
+        {
+            lock (_actions)
+            {
+                _actions.Add(action);
+            }
+            _async.Send();
+        }
+
+        private void OnAsync()
+        {
+            List<Action> actions;
+            lock (_actions)
+            {
+                actions = new List<Action>(_actions);
+                _actions.Clear();
+            }
+            foreach(var action in actions)
+            {
+                action.Invoke();
+            }
+        }
+    }
+}
+
